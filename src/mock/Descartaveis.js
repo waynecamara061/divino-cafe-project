9342a6e469b037d18c46734ce1c2d15706ff9421
Guilherme Gomes <gguife747@gmail.com>
2023-07-01 02:30:01 -0300

    Mock criado para componentes do cardapio

---
+++ src/mock/Descartaveis.js
@@ -0,0 +1,43 @@
+export const DESCARTAVEIS_MOCK = [
+  { 
+    id: 1, 
+    nome: "Copos descartáveis 200 ml", 
+    preco: 0.15, 
+    quantidade: '1' 
+  },
+  { 
+    id: 2, 
+    nome: "Copos térmicos de 60 ml", 
+    preco: 0.80, 
+    quantidade: '1' 
+  },
+  { id: 3, 
+    nome: "Copos térmicos 120 ml", 
+    preco: 1.20, 
+    quantidade: '1' 
+  },
+  { 
+    id: 4, 
+    nome: "Guardanapos",
+    preco: 8.00, 
+    quantidade: '50' 
+  },
+  { 
+    id: 5, 
+    nome: "Guardanapos refeição", 
+    preco: 12.00, 
+    quantidade: '50' 
+  },
+  { 
+    id: 6, 
+    nome: "Colheres de sobremesa", 
+    preco: 0.30, 
+    quantidade: '1' 
+  },
+  { 
+    id: 7, 
+    nome: "Misturadores de café", 
+    preco: 2.00, 
+    quantidade: '50' 
+  }
+];
---
+++ src/mock/UtlEquipamentos.js
@@ -0,0 +1,100 @@
+export const UTLEQUIPAMENTOS_MOCK = [
+  { 
+    id: 1, nome: "Toalha de banquete",
+    preco: 20.00,
+    cortesia: "Para o cliente que contrata o coffee/almoço",
+  },
+  { 
+    id: 2, 
+    nome: "Travessas de inox/vidro/louça em geral de servir",
+    preco: 10.00,
+  },
+  { 
+    id: 3, 
+    nome: "Garrafa térmica", 
+    preco: 10.00, 
+    cortesia: "Para o cliente que contrata o coffee/almoço",
+  },
+  { 
+    id: 4,
+    nome: "Rechaud",
+    preco: 30.00, 
+  },
+  { 
+    id: 5,
+    nome: "Dispenser de vidro para bebidas", 
+    preco: 30.00,
+
+  },
+  { 
+    id: 6, 
+    nome: "Samovar", 
+    preco: 30.00, 
+  },
+  { 
+    id: 7, 
+    nome: "Pratos de refeição/sobremesa", 
+    preco: 12.00, 
+    quantidade: '12', 
+  },
+  { 
+    id: 8, 
+    nome: "Xícaras de café com pires", 
+    preco: 12.00, 
+    quantidade: '12', 
+  },
+  { 
+    id: 9, 
+    nome: "Copos de vidro 220 ml", 
+    preco: 12.00, 
+    quantidade: '12', 
+  },
+  { 
+    id: 10, 
+    nome: "Facas/colheres/garfos de refeição", 
+    preco: 12.00, 
+    quantidade: '12', 
+  },
+  { 
+    id: 11, 
+    nome: "Canecas de vidro para chá, chocolate, cappuccino, etc", 
+    preco: 15.00, 
+    quantidade: '12', 
+  },
+  { 
+    id: 12, 
+    nome: "Copos de vidro especiais para sucos/água", 
+    preco: 15.00, 
+    quantidade: '12', 
+  },
+  { 
+    id: 13, 
+    nome: "Taças de vidro em relevo", 
+    preco: 14.00, 
+    quantidade: '12', 
+  },
+  { 
+    id: 14, 
+    nome: "Travessinhas de vidro", 
+    preco: 14.00, 
+    quantidade: '12', 
+  },
+  { 
+    id: 15, 
+    nome: "Ramequim", 
+    preco: 14.00, 
+    quantidade: '12' 
+  },
+  { 
+    id: 16, 
+    nome: "Colheres sobremesa/café", 
+    preco: 10.00, 
+    quantidade: '12' 
+  },
+  { 
+    id: 17, 
+    nome: "Garfinhos de sobremesa", 
+    preco: 10.00, 
+    quantidade: '12' 
+  },
+];
