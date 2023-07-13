16def6b6c5d63725d905b425ac8fa0bb3b50e049
RodrigoSobralM <rodrigo_sobral.m@hotmail.com>
2023-07-13 17:56:35 -0300

    adicionado contador aos botões

--- src/components/Banner/Header/ProductBebidas/produto.jsx
+++ src/components/Banner/Header/ProductBebidas/produto.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import './index.scss'
-const BebidasProduct = ({data, onAdicionar, onRemover}) => {
+import { useState } from 'react';
+import React from 'react';
+import './index.scss';
+
+const BebidasProduct = ({ data, onAdicionar, onRemover }) => {
+  const [counter, setCounter] = useState(0);
+
+  const decrementar = () => {
+    if (counter > 0) {
+      onRemover(data.nome);
+      setCounter(counter - 1);
+    }
+  };
+
   return (
-    
     <div className='nameDrinks'>
       <h2>{data.nome}</h2>
       <div className='nameDrinks__btn'>
         <h2>{data.preco}</h2>
-        <button  onClick={() => onRemover(data.nome)}>-</button>
-        <span>0</span>
-        <button onClick={() => onAdicionar(data.nome)}>+</button>
+        <button onClick={decrementar}>-</button>
+        <span>{counter}</span>
+        <button onClick={() => {
+          onAdicionar(data.nome);
+          setCounter(counter + 1);
+        }}>+</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BebidasProduct
+export default BebidasProduct;
