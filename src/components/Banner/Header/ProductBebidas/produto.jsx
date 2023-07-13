e9488d36b20b9ac6a747338ee27ab9df79cf092e
RodrigoSobralM <rodrigo_sobral.m@hotmail.com>
2023-07-13 14:08:59 -0300

    adicionando logica para a implementação dos pedidos em uma aba

--- src/components/Banner/Header/ProductBebidas/produto.jsx
+++ src/components/Banner/Header/ProductBebidas/produto.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './index.scss'
-const BebidasProduct = ({data}) => {
+const BebidasProduct = ({data, onAdicionar, onRemover}) => {
   return (
     
     <div className='nameDrinks'>
       <h1 >{data.nome}</h1>
       <div className='nameDrinks__btn'>
         <h1>{data.preco}</h1>
-        <button>+</button>
+        <button  onClick={() => onRemover(data.nome)}>-</button>
         <span>0</span>
-        <button>-</button>
+        <button onClick={() => onAdicionar(data.nome)}>+</button>
       </div>
     </div>
   )
--- views/Bebidas/Bebidas.jsx
+++ views/Bebidas/Bebidas.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import Header from '../../src/components/Banner/Header/Header';
 import BebidasProduct from '../../src/components/Banner/Header/ProductBebidas/produto';
@@ -10,6 +10,59 @@ function Bebidas({ data }) {
   const agua = data.filter((bebida) => bebida.tipo === 'Água');
   const leite = data.filter((bebida) => bebida.tipo === 'Leite')
   const cha = data.filter((bebida) => bebida.tipo === 'Chá')
+
+  const [itensAdicionados, setItensAdicionados] = useState([]);
+  // Função para adicionar um item
+  const adicionarItem = (nomeItem) => {
+    // Verifica se o item já foi adicionado
+    const itemExistente = itensAdicionados.find((item) => item.nome === nomeItem);
+
+    if (itemExistente) {
+      // Se o item já existe, atualiza a quantidade
+      const novosItens = itensAdicionados.map((item) => {
+        if (item.nome === nomeItem) {
+          return {
+            ...item,
+            quantidade: item.quantidade + 1,
+          };
+        }
+        return item;
+      });
+
+      setItensAdicionados(novosItens);
+    } else {
+      // Se o item não existe, adiciona um novo item ao array
+      setItensAdicionados([
+        ...itensAdicionados,
+        {
+          nome: nomeItem,
+          quantidade: 1,
+        },
+      ]);
+    }
+  };
+
+  // Função para remover um item
+  const removerItem = (nomeItem) => {
+    const novosItens = itensAdicionados.map((item) => {
+      if (item.nome === nomeItem) {
+        return {
+          ...item,
+          quantidade: item.quantidade - 1, // Altere o sinal de + para -
+        };
+      }
+      return item;
+    });
+  
+    // Remova os itens com quantidade zero
+    const itensFiltrados = novosItens.filter((item) => item.quantidade > 0);
+  
+    setItensAdicionados(itensFiltrados);
+  };
+
+  useEffect(() => {
+    console.log(itensAdicionados);
+  }, [itensAdicionados]);
   
   return (
     <div>
@@ -19,55 +72,55 @@ function Bebidas({ data }) {
         <div className='bebidas__cards'>
           <div>
             <h1>Café</h1>
+            <div className='bebidas__content'></div>
             <div className='bebidas_itens'>
-              <div className='bebidas__content'></div>
                 {cafe.map((product) => (
-                  <BebidasProduct key={product.id} data={product} />
+                  <BebidasProduct key={product.id} data={product} onRemover={removerItem} onAdicionar={adicionarItem} />
                 ))}
             </div>
           </div>
           <div>
             <h1>Leite</h1>
+            <div className='bebidas__content'></div>
             <div className='bebidas_itens'>
-              <div className='bebidas__content'></div>
                 {leite.map((product) => (
-                  <BebidasProduct key={product.id} data={product} />
+                  <BebidasProduct key={product.id} data={product} onRemover={removerItem} onAdicionar={adicionarItem} />
                 ))}
             </div>
           </div>
           <div>
             <h1>Refrigerante</h1>
+            <div className='bebidas__content'></div>
             <div className='bebidas_itens'>
-              <div className='bebidas__content'></div>
                 {refrigerante.map((product) => (
-                  <BebidasProduct key={product.id} data={product} />
+                  <BebidasProduct key={product.id} data={product} onRemover={removerItem} onAdicionar={adicionarItem} />
                 ))}
             </div>
           </div>
           <div>
             <h1>Suco</h1>
+            <div className='bebidas__content'></div>
             <div className='bebidas_itens'>
-              <div className='bebidas__content'></div>
                 {suco.map((product) => (
-                  <BebidasProduct key={product.id} data={product} />
+                  <BebidasProduct key={product.id} data={product} onRemover={removerItem} onAdicionar={adicionarItem} />
                 ))}
             </div>
           </div>
           <div>
             <h1>Água</h1>
+            <div className='bebidas__content'></div>
             <div className='bebidas_itens last-item'>
-              <div className='bebidas__content'></div>
                 {agua.map((product) => (
-                  <BebidasProduct key={product.id} data={product} />
+                  <BebidasProduct key={product.id} data={product} onRemover={removerItem} onAdicionar={adicionarItem} />
                 ))}
             </div>
           </div>
           <div>
             <h1>Chá</h1>
+            <div className='bebidas__content'></div>
             <div className='bebidas_itens last-item'>
-              <div className='bebidas__content'></div>
                 {cha.map((product) => (
-                  <BebidasProduct key={product.id} data={product} />
+                  <BebidasProduct key={product.id} data={product} onRemover={removerItem} onAdicionar={adicionarItem} />
                 ))}
             </div>
           </div>
